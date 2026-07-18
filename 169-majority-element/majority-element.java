c852657ec1dd7793be260d3ae5bb4f41180a5efa
Priyanshu Yadav <161307982+trpyyadav@users.noreply.github.com>
2026-07-18 14:05:03 +0530

    Time: 16 ms (20.56%) | Memory: 52.5 MB (90.88%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int maxElement = Integer.MIN_VALUE;
+
+        for(int num: nums){
+            map.put(num, map.getOrDefault(num, 0)+1);
+
+            if(map.get(num) > nums.length/2){
+                return num;
+            }
+        }
+
+        return -1;
+        
+    }
+}
